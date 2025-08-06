b287cf4537452cd8ff84c7fcc7c073a36513f588
JankoooGH <jasiek32100@gmail.com>
2025-08-07 00:59:26 +0200

    Transakcje, Balans, Lista

--- Budget_manager.py
+++ Budget_manager.py
@@ -1,4 +1,4 @@
-
+import json
 
 class Transaction:
     def __init__(self, amount, category, date, t_type):
@@ -8,8 +8,22 @@ class Transaction:
         self.t_type = t_type
 
 
+class BudgetManager:
 
+    def __init__(self):
+        self.transaction = []
 
+    def add_transaction(self, transaction):
+        self.transaction.append(transaction)
+
+    def get_balance(self):
+        income = sum(t.amount for t in self.transaction if t.t_type == 'income')
+        expense = sum(t.amount for t in self.transaction if t.t_type == 'expense')
+        return income - expense
+
+    def list_transaction(self):
+        for t in self.transaction:
+            print(f"{t.date} | {t.category} | {t.amount} | {t.t_type}")
 
 
 while True:
@@ -25,6 +39,8 @@ while True:
         category = input('Kategoria: ')
         date = input('Data: ')
         t_type = input('Rodzaj transakcji (dochody/wydatki): ')
+        transaction = Transaction(amount, category, date, t_type)
+
     elif opcja == 2:
         print('xyz')
     elif opcja == 3:
